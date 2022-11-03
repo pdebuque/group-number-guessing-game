b9a2ec34da9216f6dfd30f1cd0a1c1b7e27dc0dd
Paolo Debuque <pdebuque@gmail.com>
2022-11-03 10:16:04 -0500

    baseline server/client communication established

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,55 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  $('#submit-guesses').on('click', postGuesses)
+
+}
+
+// create the req object from the input values, send them as data in a POST request
+function postGuesses() {
+  console.log('in postGuesses()');
+  $.ajax({
+    method: 'POST',
+    url: '/submit',
+    data: {
+      player1: $('#player-1-input').val(),
+      player2: $('#player-2-input').val()
+    }
+  }).then((res) => {
+    console.log('POST successful: ', res);
+
+    // fire a different function to render display
+  }).catch((error) => {
+    console.log('something went wrong: ', error)
+  });
+
+  getGuesses();
+  // clear inputs
+  $('#player-1-input').val('')
+  $('#player-2-input').val('')
+
+}
+
+function getGuesses() {
+  console.log('in getGuesses()');
+  $.ajax({
+    method: 'GET',
+    url: '/guesses'
+  }).then((res) => {
+    renderDisplay(res)
+  })
+  // GET request to /guesses
+  // retrieve all previous guesses
+
+
+}
+// function to render the display element
+
+function renderDisplay(array) {
+  $('#display').empty();
+  for (let round of array) {
+    $('#display').append(`
+      Player 1 guess: ${round.player1}; Player 2 guess: ${round.player2}
+    `)
+  }
 }
--- server/server.js
+++ server/server.js
@@ -3,15 +3,44 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
+// app.use(express.json)
+
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
+const guesses = [];
+//set up the random number. this will reset when the server reloads
+const randomNum = Math.floor(Math.random() * 26)
+
 // GET & POST Routes go here
 
+// GET: send the array of guesses
+
+
+
+// POST: push the guesses into the array
+// req: object with guesses in it, e.g, {player1: <num>, player2: <num>}
+app.post('/submit', (req, res) => {
+  // console.log('new guesses: ', req);
+  guesses.push(req.body);
+  console.log(`-------------- GUESSES SO FAR:`, guesses, `-------------------`)
+
+  // check guesses
+  res.sendStatus(200);
+})
+
+app.get('/guesses', (req, res) => {
+  console.log('GET request at /guesses', guesses);
+  res.send(guesses);
+})
 
 app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
+  console.log('Server is running on port', PORT)
 })
+
+
+
+
